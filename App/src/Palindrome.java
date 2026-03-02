4f99b7e79c3e951b9fde80055db72e27486dae62
Netraveejay <netraveejay2006@gmail.com>
2026-03-02 10:38:20 +0530

    uc8 completed

--- App/src/Palindrome.java
+++ App/src/Palindrome.java
@@ -1,24 +1,71 @@
 public class Palindrome {
+    static class Node {
+        char data;
+        Node next;
+
+        Node(char data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
      static void main() {
          String input = "madam";
 
-         Deque<Character> deque = new LinkedList<>();
+         Node head = null, tail = null;
 
          for (int i = 0; i < input.length(); i++) {
-             deque.addLast(input.charAt(i));
-         }
-
-         boolean isPalindrome = true;
-
-         while (deque.size() > 1) {
-             if (deque.removeFirst() != deque.removeLast()) {
-                 isPalindrome = false;
-                 break;
+             Node newNode = new Node(input.charAt(i));
+             if (head == null) {
+                 head = newNode;
+                 tail = newNode;
+             } else {
+                 tail.next = newNode;
+                 tail = newNode;
              }
          }
 
+         boolean isPalindrome = isPalindrome(head);
+
          System.out.println("Input String : " + input);
          System.out.println("Is it a Palindrome? : " + isPalindrome);
+
+     }
+    public static boolean isPalindrome(Node head) {
+
+        if (head == null || head.next == null)
+            return true;
+
+        Node slow = head;
+        Node fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        Node prev = null;
+        Node current = slow;
+        Node nextNode;
+
+        while (current != null) {
+            nextNode = current.next;
+            current.next = prev;
+            prev = current;
+            current = nextNode;
+        }
+
+        Node firstHalf = head;
+        Node secondHalf = prev;
+
+        while (secondHalf != null) {
+            if (firstHalf.data != secondHalf.data) {
+                return false;
+            }
+            firstHalf = firstHalf.next;
+            secondHalf = secondHalf.next;
+        }
+
+        return true;
     }
 }
 
